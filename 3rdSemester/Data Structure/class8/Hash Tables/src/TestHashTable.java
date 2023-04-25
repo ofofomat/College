f981d7ad9a4ce508b6e6a85b6980e528f6e9ded7
MATEUS FERNANDES <mateus.fernandes@catolicasc.edu.br>
2023-04-24 21:13:01 -0300

    Changed some java files

--- 3rdSemester/Data Structure/class8/Hash Tables/src/TestHashTable.java
+++ 3rdSemester/Data Structure/class8/Hash Tables/src/TestHashTable.java
@@ -1,15 +1,37 @@
 public class TestHashTable {
     public static void main(String[] args) {
         HashTable table = new HashTable();
-		table.add("palavra");
-		table.add("computador");
-		table.add("apostila");
-		table.add("instrutor");
-		table.add("mesa");
-		table.add("telefone");
-		table.add("telefonia");
-		table.add("#Teste");
-		table.add("*Teste2");
+		table.add("A");
+		table.add("B");
+		table.add("C");
+		table.add("D");
+		table.add("E");
+		table.add("F");
+		table.add("G");
+		table.add("H");
+		table.add("I");
+		table.add("J");
+		table.add("K");
+		table.add("L");
+		table.add("M");
+		table.add("N");
+		table.add("O");
+		table.add("P");
+		table.add("Q");
+		table.add("R");
+		table.add("S");
+		table.add("T");
+		table.add("U");
+		table.add("V");
+		table.add("W");
+		table.add("X");
+		table.add("Y");
+		table.add("Z");
+		table.add("á");
+		table.add("*");
+		table.add("0");
+
+		
 		
 		System.out.println(table.count());
 		System.out.println(table.getAll());
