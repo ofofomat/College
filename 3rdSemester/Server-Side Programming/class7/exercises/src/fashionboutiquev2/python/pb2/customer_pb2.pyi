bb9749911e71bb6523600191d28c3d5b03bc8343
Mateus Fernandes <mateusowlfernandes@gmail.com>
2023-04-27 11:41:25 -0300

    Created the proto file and the grpc python files

---
+++ 3rdSemester/Server-Side Programming/class7/exercises/src/fashionboutiquev2/python/pb2/customer_pb2.pyi
@@ -0,0 +1,50 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Customer(_message.Message):
+    __slots__ = ["email", "id", "nome"]
+    EMAIL_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    NOME_FIELD_NUMBER: _ClassVar[int]
+    email: str
+    id: Id
+    nome: str
+    def __init__(self, id: _Optional[_Union[Id, _Mapping]] = ..., nome: _Optional[str] = ..., email: _Optional[str] = ...) -> None: ...
+
+class Id(_message.Message):
+    __slots__ = ["id"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: int
+    def __init__(self, id: _Optional[int] = ...) -> None: ...
+
+class ListOfCustomer(_message.Message):
+    __slots__ = ["customer"]
+    CUSTOMER_FIELD_NUMBER: _ClassVar[int]
+    customer: _containers.RepeatedCompositeFieldContainer[Customer]
+    def __init__(self, customer: _Optional[_Iterable[_Union[Customer, _Mapping]]] = ...) -> None: ...
+
+class Request(_message.Message):
+    __slots__ = ["request"]
+    REQUEST_FIELD_NUMBER: _ClassVar[int]
+    request: int
+    def __init__(self, request: _Optional[int] = ...) -> None: ...
+
+class Response(_message.Message):
+    __slots__ = ["http_status_code", "name", "operation"]
+    class Type(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+        __slots__ = []
+    ADD: Response.Type
+    ALTER: Response.Type
+    DELETE: Response.Type
+    HTTP_STATUS_CODE_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    OPERATION_FIELD_NUMBER: _ClassVar[int]
+    http_status_code: int
+    name: str
+    operation: Response.Type
+    def __init__(self, operation: _Optional[_Union[Response.Type, str]] = ..., name: _Optional[str] = ..., http_status_code: _Optional[int] = ...) -> None: ...
