d0da1172c1436e2524772ace95fc70a563cf8802
MATEUS FERNANDES <mateus.fernandes@catolicasc.edu.br>
2023-04-27 20:08:37 -0300

    Started editing the client side of the grpc application

--- 3rdSemester/Server-Side Programming/class7/exercises/src/fashionboutiquev2/python/customer_client.py
+++ 3rdSemester/Server-Side Programming/class7/exercises/src/fashionboutiquev2/python/customer_client.py
@@ -4,3 +4,14 @@ import random
 
 import grpc
 from pb2 import customer_pb2, customer_pb2_grpc
+
+
+def run():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = customer_pb2_grpc.CustomerListStub(channel)
+        print("SOMETHING HERE")
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    run()
