8b5fb9b88ccdd813797f0d1c28fa4cf282da5bed
Mateus Fernandes <mateusowlfernandes@gmail.com>
2023-04-30 12:15:21 -0300

    Failed attempt to create a grpc api. Version two coming this week.

--- 3rdSemester/Server-Side Programming/class7/exercises/src/fashionboutiquev2/python/customer_client.py
+++ 3rdSemester/Server-Side Programming/class7/exercises/src/fashionboutiquev2/python/customer_client.py
@@ -36,30 +36,30 @@ def call_alter_a_customer(stub):
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = customer_pb2_grpc.CustomerListStub(channel)
-        print("---------- AddCustomer1 ----------")
-        call_add_a_customer(stub)
+        # print("---------- AddCustomer1 ----------")
+        # call_add_a_customer(stub)
         # SHOULD ADD ONE CUSTOMER
-        print("---------- AddCustomer2 ----------")
-        call_add_a_customer(stub)
-        # should add another customer
-        print("---------- AddCustomer3 ----------")
-        call_add_a_customer(stub)
-        # should add the last customer
-        print("---------- DeleteCustomer ----------")
-        call_delete_a_customer(stub)
-        # should delete the second customer
-        print("---------- AlterCustomer ----------")
-        call_alter_a_customer(stub)
-        # should alter the first customer
-        print("---------- GetCustomer ----------")
-        call_get_a_customer(stub)
-        # should return customer 1
-        print("---------- GetCustomer ----------")
-        call_get_a_customer(stub)
-        # should return customer 2
-        # and it should be equal to the last customer added
-        print("---------- GetAllCustomers ----------")
-        call_get_all_customers(stub)
+        # print("---------- AddCustomer2 ----------")
+        # call_add_a_customer(stub)
+        # # should add another customer
+        # print("---------- AddCustomer3 ----------")
+        # call_add_a_customer(stub)
+        # # should add the last customer
+        # print("---------- DeleteCustomer ----------")
+        # call_delete_a_customer(stub)
+        # # should delete the second customer
+        # print("---------- AlterCustomer ----------")
+        # call_alter_a_customer(stub)
+        # # should alter the first customer
+        # print("---------- GetCustomer ----------")
+        # call_get_a_customer(stub)
+        # # should return customer 1
+        # print("---------- GetCustomer ----------")
+        # call_get_a_customer(stub)
+        # # should return customer 2
+        # # and it should be equal to the last customer added
+        # print("---------- GetAllCustomers ----------")
+        # call_get_all_customers(stub)
         # should have 2 customers
 
 
