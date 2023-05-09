ce98180ac0200a6815238590f8edb8e101d4d436
MATEUS FERNANDES <mateus.fernandes@catolicasc.edu.br>
2023-05-09 20:23:40 -0300

    Done lots of things

--- Server-Side Programming/class8/server-graphQL/src/app.py
+++ Server-Side Programming/class8/server-graphQL/src/app.py
@@ -14,3 +14,7 @@ app.add_url_rule(
     '/graphql',
     view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
 )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port='5000', host='0.0.0.0')
